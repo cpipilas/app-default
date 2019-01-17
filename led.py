ebe34418daadb0777c00c36e385945741e908d1c
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-01-17 15:41:25 +0200

    Create led.py

---
+++ led.py
@@ -0,0 +1,12 @@
+from hardware import gpio
+
+class LED:
+        def __init__(self, pinid, onLevel):
+                self.onLevel = onLevel
+                self.pinid = pinid
+                self.gpio = gpio(pinid, gpio.GPIO_MODE_OUTPUT_PULLDOWN)
+                self.gpio.write(1 - self.onLevel)
+        def ON(self):
+                self.gpio.write(self.onLevel)
+        def OFF(self):
+                self.gpio.write(1 - self.onLevel)
